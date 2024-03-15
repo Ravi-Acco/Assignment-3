c73aee0f7266b15be9251decad77cd6f5dfeb10e
Ravi <ravi.prasad@accolitedigital.com>
2024-03-15 21:26:59 +0530

    Assignment Java Core done

--- Core/Exercise1.java
+++ Core/Exercise1.java
@@ -2,13 +2,30 @@ import java.util.*;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(5, 2, 9, 1, 7, 3);
+        System.out.println("Collections in java");
 
-        Set<Integer> sortedSet = new TreeSet<>(numbers);
-
-        System.out.println("Sorted list of integers:");
-        for (int num : sortedSet) {
-            System.out.println(num);
+        //Making a normal array first
+        System.out.println("Printing the array: ");
+        int rand[]={21,69,47,52,99};
+        for (int i : rand) {
+            System.out.print(i);
+            System.out.print(" ");
         }
+        
+        //Converting the array to arraylist
+        ArrayList<Integer> l1=new ArrayList<>();
+        for (Integer i : rand) {
+            l1.add(i);
+        }
+        System.out.println("\nPrinting the ArrayList");
+        System.out.println(l1);
+
+        //Converting the ArrayList to SortedSet
+        SortedSet<Integer> Sortedl1 = new TreeSet<Integer>(l1);
+        
+        System.out.println("Printing the SortedSet");
+        System.out.println(Sortedl1);
+
+
     }
 }
--- Core/Exercise2.java
+++ Core/Exercise2.java
@@ -1,34 +1,44 @@
+import java.util.Scanner;
 
 public class Exercise2 {
+    public static int div(int numerator, int denominator) throws ArithmeticException{
+        if(denominator==0){
+            throw new ArithmeticException("Cannot divide by zero exception from method div");
+        }
+        return numerator/denominator;
+    }
     public static void main(String[] args) {
+        System.out.println("Enter two numbers : ");
+        Scanner sc=new Scanner(
+            System.in
+        );
+
+        int a=sc.nextInt();
+
+        int b=sc.nextInt();
+        int c = -1;
+        try{
+            c=a/b;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Is it possible to divide by zero?");
+        }
+        catch(Exception e){
+            System.out.println("Any other exception");
+        }
+        finally{
+            sc.close();
+            if(c != -1){
+                System.out.println("The value of division is " + c);
+            }
+            else System.out.println("The value of c is not defined");
+        }
+
         try {
-            divideByZero();
-        } catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception caught: " + e.getMessage());
+            int result=div(a,b);
+            System.out.println("Result is " + result);
+        } catch (Exception e) {
+            System.out.println("Exception called after function execution from main");
         }
-    }
-    // using throw 
-    public static void divideByZero() {
-        int numerator = 10;
-        int denominator = 0;
-
-        if (denominator == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-
-        int result = numerator / denominator;
-        System.out.println("Result: " + result);
-    }
-    // using throws
-    public static void divideByZeroWithThrows() throws ArithmeticException {
-        int numerator = 10;
-        int denominator = 0;
-
-        if (denominator == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-
-        int result = numerator / denominator;
-        System.out.println("Result: " + result);
     }
 }
